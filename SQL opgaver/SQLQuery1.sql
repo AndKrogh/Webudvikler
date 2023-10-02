00925bbe656e1a867a5b5fe4c7f5dc51a7a8cd70
AndKrogh <anders.krogh99@gmail.com>
2023-10-02 11:14:01 +0200

    adad

--- SQL opgaver/SQLQuery1.sql
+++ SQL opgaver/SQLQuery1.sql
@@ -10,4 +10,12 @@ SELECT LifeExpectancy, Name
 FROM country
 ORDER BY LifeExpectancy asc;
 
+USE world;
+
+SELECT region, SUM(population) AS SumPerRegion
+FROM country
+GROUP BY region
+HAVING SUM(population) < 8000000
+ORDER BY SumPerRegion asc;
+
 select * from Country;
