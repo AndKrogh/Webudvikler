47cafb631aa48426ad7b352887bf55c3b281a825
AndKrogh <anders.krogh99@gmail.com>
2023-10-02 11:11:38 +0200

    esf

---
+++ SQL opgaver/SQLQuery1.sql
@@ -0,0 +1,13 @@
+USE world;
+
+SELECT region, SurfaceArea 
+FROM country
+ORDER BY SurfaceArea DESC;
+
+USE world;
+
+SELECT LifeExpectancy, Name 
+FROM country
+ORDER BY LifeExpectancy asc;
+
+select * from Country;
