e80c866d2153cb59ec8cd9fe65effe32634cff98
AndKrogh <anders.krogh99@gmail.com>
2023-10-06 10:18:17 +0200

    efwf

---
+++ library.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+    
+    myPromise: function (resource) {
+        new Promise((resolve,reject) => {
+            fetch(`https://jsonplaceholder.typicode.com/${resource}`,{})
+            .then((res)=> res.json())
+            /* .then((res) => {
+                return res.json();
+            }) */
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log('catch', err);
+            });
+        });
+    }
+};
+
+/* function myPromise (resource) {
+    new Promise((resolve,reject) => {
+        fetch(`https://jsonplaceholder.typicode.com/${resource}`,{})
+        .then((res)=> res.json())
+        .then((res) => {
+            return res.json();
+        })
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log('catch', err);
+        });
+    });
+} */
--- oprettelseServer/index.js
+++ oprettelseServer/index.js
@@ -1,18 +1,3 @@
+const lib = require('../library.js');
 
-function myPromise (resource) {
-    new Promise((resolve,reject) => {
-        fetch(`https://jsonplaceholder.typicode.com/${resource}`,{})
-        .then((res)=> res.json())
-        /* .then((res) => {
-            return res.json();
-        }) */
-        .then((res) => {
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log('catch', err);
-        });
-    });
-}
-
-myPromise( 'posts');
+lib.myPromise( 'posts');
