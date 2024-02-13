36bf8fde1c8b45355531472f1d4064dd484f3f45
AndKrogh <anders.krogh99@gmail.com>
2024-02-13 11:10:48 +0100

    making old assignments

---
+++ vue-opgaver/src/components/Template.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+
+//Model
+const isClicked = ref(false);
+
+//ViewModel
+const clickButton = ( ) => {
+    isClicked.value = true;
+};
+
+
+</script>
+
+<template>
+    <div>
+        {{ 4 > 5 ? 'Større end' : 'Mindre end' }}
+        <hr>
+        <p v-bind:class =" {active: isClicked}">
+            skift state hvis klikket
+        </p>
+        <button @click="clickButton">click it</button>
+        {{ isClicked && 'Nu har du klikket på knappen...' }}
+        <p v-if="isClicked">
+            Nu har du klikket på knappen
+        </p>
+         <p v-else-if="!isClicked">
+            Dette er ikke true state
+        </p>
+         <p v-else>
+            Denne er meget svær at nå til
+        </p>
+    </div>
+
+</template>
---
+++ vue-opgaver/src/components/User_input.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+const inputText = ref(false);
+const textArray = ref([]);
+
+const messages = ref("");
+
+//ViewModel 
+const clickButton = () => {
+    inputText.value = true;
+    textArray.value.push(messages.value);
+}
+
+</script>
+
+<template>
+    
+    <div>
+        <input type="text" v-model="messages" placeholder="text">
+        <button @click="clickButton"> send</button>
+        <hr>
+        <p v-if="inputText">
+            {{ messages }}
+        </p>
+        <hr>
+        <ul>
+            <li v-for="text in textArray">
+                {{ text }}
+            </li>
+        </ul>
+    </div> 
+</template>
