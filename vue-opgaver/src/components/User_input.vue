87ca9b9ceecc70191e9f74c912ecb63c44301a75
AndKrogh <anders.krogh99@gmail.com>
2024-02-14 13:12:47 +0100

    making task 3

--- vue-opgaver/src/components/User_input.vue
+++ vue-opgaver/src/components/User_input.vue
@@ -1,22 +1,42 @@
 <script setup>
 import { ref } from 'vue';
 
-const inputText = ref(false);
-const textArray = ref([]);
+const savedArr = localStorage.length;
+console.log(savedArr);
+const savedMsg = localStorage.getItem(savedArr + 1);
+console.log(savedMsg);
+
+const inputText = ref();
+const textArray = ref([savedMsg]);
+
+
 
 const messages = ref("");
+
+
 
 //ViewModel 
 const clickButton = () => {
     inputText.value = true;
     textArray.value.push(messages.value);
+    console.log(textArray.value.length);   
+    for (let i = 0; i < textArray.value.length; i++) {
+        console.log(textArray.value[i]);
+        localStorage.setItem(textArray.value.length, textArray.value[i]);
+    }
+    
 
     messages.value = "";
 }
 
 const deleteItem = (index) => {
-    console.log(index);
-    console.log(textArray.value);
+    console.log(savedArr);
+    textArray.value.splice(index);
+    localStorage.removeItem(savedArr + index);
+};
+
+const clearAll = (index) => {
+    localStorage.clear();
     textArray.value.splice(index);
 };
 
@@ -27,17 +47,18 @@ const deleteItem = (index) => {
     
     <div>
         <input type="text" v-model="messages" placeholder="text">
-        <button @click="clickButton"> send</button>
+        <button @click="clickButton(id)"> send</button>
         <hr>
         <p v-if="inputText">
             {{ messages }}
         </p>
         <hr>
-        <ul>
+        <ul >
             <li v-for="(text, index) in textArray" >
-                {{ text }}
-                <button @click="deleteItem(index)">Delete</button>
+                <p> {{ text }} <button @click="deleteItem(index)">Delete</button></p>
+                
             </li>
         </ul>
+        <button @click="clearAll(index)">Clear all</button>
     </div> 
 </template>
