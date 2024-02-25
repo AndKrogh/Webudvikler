ffa4fa1ff6ec0b9814f56c1e1bcbe1b2e6af9f01
AndKrogh <anders.krogh99@gmail.com>
2024-02-25 12:44:34 +0100

    vue routing

--- mfd_4_vue/vue-routing/src/views/UserProfileView.vue
+++ mfd_4_vue/vue-routing/src/views/UserProfileView.vue
@@ -1,8 +1,41 @@
 <template>
-    <h1>This is an user page</h1>
+    <div v-for="user in users" :key="user.firstname">
+    <h1>welcome {{ $route.params.userid}}</h1>
     <p>The user is {{ $route.params.userid }}</p>
+    <router-link :to="{ name: 'userProfileView', params: {userid: user.firstname} }"> click </router-link>
+    </div>
+   
 </template>
 
 <script setup>
+
+ const users = 
+ {
+	"nf2sjb19u7": {
+		"email": "john@doe.com",
+		"firstname": "John",
+		"lastname": "Doe"
+	},
+	"nfkx6cywbs": {
+		"email": "jane@doe.com",
+		"firstname": "Jane",
+		"lastname": "Doe"
+	},
+	"6auit2k477": {
+		"email": "mdam@ucl.dk",
+		"firstname": "Morten",
+		"lastname": "Damgaard"
+	},
+	"a6uqet4p3c": {
+		"email": "kjcl@ucl.dk",
+		"firstname": "Kenneth",
+		"lastname": "Clausen"
+	},
+	"kaqbxhoadq": {
+		"email": "jvje@ucl.dk",
+		"firstname": "Jakob",
+		"lastname": "Jensen"
+	}
+}
 
 </script>
