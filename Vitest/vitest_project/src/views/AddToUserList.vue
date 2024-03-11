e744a84c37ef32ae1a351b237bfceb6b0e68ea73
AndKrogh <anders.krogh99@gmail.com>
2024-03-11 14:11:00 +0100

    trying to make task work

---
+++ Vitest/vitest_project/src/views/AddToUserList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input v-model="newUserName" type="text" placeholder="Enter new user name" />
+    <button @click="addUser">Add User</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const users = ref([]);
+const newUserName = ref("");
+
+const addUser = () => {
+   users.value.push({  name: newUserName.value.trim() });
+   console.log(users);
+};
+
+</script>
---
+++ Vitest/vitest_project/src/views/User_list.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>User List</h2>
+    <div v-for="(user, index) in users" :key="user.name">
+      <div>
+        <span>{{ user.name }}</span>
+        <button @click="editUser(index)">Edit</button>
+        <button @click="deleteUser(index)">Delete</button>
+      </div>
+    </div>
+    <addUser :users ="users"/>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import addUser from "./AddToUserList.vue"
+
+const users = ref([
+
+]);
+
+console.log(users);
+
+
+const deleteUser = (index) => {
+  users.value.splice(index, 1);
+};
+
+const editUser = (index) => {
+  const newName = prompt("Enter new name:", users.value[index].name);
+  if (newName !== null && newName.trim() !== "") {
+    users.value[index].name = newName.trim();
+  }
+};
+</script>
+
+<style scoped>
+/* Add your styles here if needed */
+</style>
